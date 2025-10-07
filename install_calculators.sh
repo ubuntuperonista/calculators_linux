762ba42d2d5f7a3b548611f0e7be90ed3f4763d6
ubuntuperonista <30541016+ubuntuperonista@users.noreply.github.com>
2025-10-07 19:06:39 -0300

    Create install_calculators.sh
    Create installer

---
+++ install_calculators.sh
@@ -0,0 +1,107 @@
+## Install HP-15c emulator on Linux as per instucted in https://ubuntuperonista.blogspot.com/2021/12/como-instalo-emulador-calculadora-hp-ubuntu.html
+mkdir /tmp/hp15c/ ;
+cd /tmp/hp15c/ ;
+wget  https://bit.ly/3cMWTH8 -O HP-15C_4.3.00_Linux_x86_64.zip ;
+unzip HP-15C_4.3.00_Linux_x86_64.zip ;
+chmod +x /tmp/hp15c/HP-15C ;
+cp /tmp/hp15c/HP-15C_Simulator_Font.ttf ~/.local/share/fonts/ ;
+fc-cache -f ;
+cd /tmp ;
+mv /tmp/hp15c/ ~/.hp15c/ ;
+cd ~/.hp15c/doc ;
+wget http://h10032.www1.hp.com/ctg/Manual/c00503576.pdf -O hp11c_owners_manual_and_problem_solving_guide.pdf ;
+wget http://h10032.www1.hp.com/ctg/Manual/c03030589.pdf -O hp15c_deluxe_owners_handbook.pdf ;
+cd ~
+
+## Install free42 HP-42 clone emulator on Ubuntu:
+sudo apt update
+sudo apt upgrade
+sudo apt install free42-nologo
+
+## Install Plus42 HP-42 clone on Linux
+cd /tmp/ ;
+wget https://thomasokken.com/plus42/download/Plus42Linux.tgz ;
+tar xvfz Plus42Linux.tgz
+mv /tmp/Plus42Linux/ ~/.plus42/
+mkdir $HOME/.local/share/plus42 ;
+cd $HOME/.local/share/plus42 ;
+wget https://thomasokken.com/plus42/skins/desktop/Mueck_P42d.zip ;
+wget https://thomasokken.com/plus42/skins/desktop/Mueck_P42d.gif ;
+wget https://thomasokken.com/plus42/skins/desktop/Mueck_P42d.layout ;
+wget https://thomasokken.com/plus42/skins/desktop/Mueck_P42d_MN.zip ;
+wget https://thomasokken.com/plus42/skins/desktop/Mueck_P42d_MN.gif ;
+wget https://thomasokken.com/plus42/skins/desktop/Mueck_P42d_MN.layout ;
+unzip -fo Mueck_P42d.zip  ;
+unzip -fo Mueck_P42d_MN.zip
+
+## Install programs for your Plus42:
+mkdir ~/.plus42/prg/
+cd  ~/.plus42/prg
+wget https://thomasokken.com/free42/42progs/plot_orig.raw
+wget https://thomasokken.com/free42/42progs/plot_solve.raw
+wget https://thomasokken.com/free42/42progs/dplot_orig.raw
+wget https://thomasokken.com/free42/42progs/dplot_solve.raw
+wget https://thomasokken.com/free42/42progs/sun.raw
+wget https://thomasokken.com/free42/42progs/prm_dec.raw
+wget https://thomasokken.com/free42/42progs/prm_bin.raw
+wget https://thomasokken.com/free42/42progs/stress.raw
+wget https://thomasokken.com/free42/42progs/synth.raw
+wget https://thomasokken.com/free42/42progs/mast.raw
+wget https://thomasokken.com/free42/42progs/minehunt.raw
+wget https://thomasokken.com/free42/42progs/OkieDokie/okiedokie.raw
+wget https://thomasokken.com/free42/42progs/rtmin.raw
+wget https://thomasokken.com/free42/42progs/RootsEqns.raw
+wget https://thomasokken.com/free42/42progs/mlran.raw
+wget https://thomasokken.com/free42/42progs/aes.raw
+wget https://thomasokken.com/free42/42progs/aesprep.raw
+wget https://thomasokken.com/free42/42progs/Base.raw
+wget https://thomasokken.com/free42/42progs/Convert.raw
+wget https://thomasokken.com/free42/42progs/Finance.raw
+wget https://thomasokken.com/free42/42progs/Triangle.raw
+wget https://thomasokken.com/free42/42progs/ses.raw
+wget https://thomasokken.com/free42/42progs/G4_G3_G2.raw
+wget https://thomasokken.com/free42/42progs/STATISTICS.raw
+wget https://thomasokken.com/free42/42progs/ntrp.raw
+wget https://thomasokken.com/free42/42progs/lrnc.raw
+wget https://thomasokken.com/free42/42progs/mort.raw
+wget https://thomasokken.com/free42/42progs/Moody42/Moody42.raw
+wget https://thomasokken.com/free42/42progs/Astronavigatie/astro_tso.raw
+wget https://thomasokken.com/free42/42progs/Astronavigatie/astra_tso.raw
+wget https://thomasokken.com/free42/42progs/Astronavigatie/avsho.raw
+wget https://thomasokken.com/free42/42progs/unixtime.raw
+wget https://thomasokken.com/free42/42progs/moeglein/DO_CALC.raw
+wget https://thomasokken.com/free42/42progs/moeglein/N_CAL.raw
+wget https://thomasokken.com/free42/42progs/Twix/twix.raw
+wget https://thomasokken.com/free42/42progs/cp2oct.raw
+wget https://thomasokken.com/free42/42progs/Convert2.raw
+
+## Install HP48 clone emulator on Ubuntu Linux:
+####Install dependencies:
+sudo apt install git curl libpkgconf3 libreadline-dev libsdl2-dev libx11-dev libxext-dev liblua5.4-dev
+
+### Install code an compiles it:
+cd /tmp
+git clone https://github.com/gwenhael-le-moine/x48ng
+cd /tmp/x48ng
+make
+sudo make install PREFIX=/usr
+/usr/share/x48ng/setup-x48ng-home.sh
+
+x48ng --print-config > ~/.config/x48ng/config.lua
+sed ~/.config/x48ng/config.lua s/"\"rom = \"rom\""/"\"rom = \"ROMs/gxrom-r\""
+
+## Install WP43S WP43 calculator emulator on Linux
+### Install dependencies
+sudo apt-get install meson git ninja-build pkg-config build-essential ccache libgtk-3-dev libgmp-dev libpulse-dev gcc-arm-none-eabi
+
+###Download code, compiles it, and creates a launcher
+cd /tmp ;
+git clone https://gitlab.com/rpncalculators/wp43
+cd /tmp/wp43/ ;
+make clean ;
+make ;
+cd /tmp ;
+mv wp43/ ~/.wp43/
+echo "cd ~/.wp43/build.sim/src/wp43-gtk/" > ~/.wp43.sh
+echo "./wp43" >> ~/.wp43.sh 
+chmod +x ~/.wp43.sh
